d2dc96ab53cb8dc35f761743c9b0b1890d81cbd5
unknown <bb3583400@gmail.com>
2023-03-25 23:12:12 +0600

    a

--- src/components/SingleLetter.js
+++ src/components/SingleLetter.js
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react'
 import ReactToPrint from 'react-to-print';
+import './SingleLetter.css'
 
 
 const SingleLetter = ({item, index}) => {
@@ -24,8 +25,31 @@ const deleteProduct2 = async(id)=>{
 
   return (
     <div className='print' >
+
         <ul key={item._id}>
             <div ref={ref} className='letter-area'>
+                <div className='header-area'>
+                    <div className='letter-logo'>
+                       <h1>LOGO</h1>
+                    </div>
+                    <div className='header-text'>
+                      <h1>মাদারীপুর পল্লী বিদ্যুৎ সমিতি</h1>
+                      <h3>শিবচর জোনাল অফিস</h3>
+                      <h4>শিবচর, মাদারীপুর।</h4>
+                    </div>
+
+ 
+                </div>
+                <hr/>
+ <p>স্মারক নং - ----------------------------------------------------------------------------------------তারিখঃ <span>{name}</span></p> 
+                <div className='name-address'>
+                  <p>নামঃ {name}</p>
+                  <p>পিতার নামঃ {}</p>
+                  <p>গ্রামঃ {} <span>ডাকঘরঃ {}</span></p>
+                  <p>উপজেলাঃ {} <span>জেলাঃ {}</span></p>
+                  <p>মোবাইল নম্বরঃ {}</p>
+                </div>
+                
                 <li>{index+1}</li> 
                 <li>{name}</li>
                 <li>{price}</li>
